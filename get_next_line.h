7fbd71d01ecfcd2b0e144716082dcc9c64888d47
Chaks42 <chacal_2222@hotmail.com>
2025-01-06 21:44:37 +0100

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trouilla <trouilla@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 17:10:57 by trouilla          #+#    #+#             */
+/*   Updated: 2024/11/06 18:45:13 by trouilla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strchr(const char *s, char c);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *src);
+
+#endif
