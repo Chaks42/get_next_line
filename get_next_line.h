0a59780b2c4e9df9cc84bcba862f83af0bf4b294
Chaks42 <chacal_2222@hotmail.com>
2025-01-07 15:04:03 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -20,6 +20,11 @@
 #  define BUFFER_SIZE 42
 # endif
 
+# if BUFFER_SIZE > trouver la taille
+#  ndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 int		ft_strchr(const char *s, char c);
